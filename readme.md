23f01898243c83016124fa4f89a6bc51f920ecf3
mosowe <630834853@qq.com>
2021-10-19 10:30:09 +0800

    优化

--- readme.md
+++ readme.md
@@ -8,8 +8,22 @@
 > 2. 热模块替换
 > 3. 按需编译
 
+## 功能
+    1. antd-mobile移动端组件
+    2. axios网络数据交互
+    3. hox状态管理
+    4. react-router-dom路由管理
+    5. postcss-px-to-viewport移动端px转vw/vh
+    6. less预编译
+    7. autoprefixer自动补全
+    8. typescript语法
+
 ## 使用
 
-1.  下载代码
+1.  下载`git clone`
 2.  `npm install`
+3.  `npm run dev`
 
+## 更多
+
+[项目博客地址](https://blog.csdn.net/skyblacktoday/article/details/120827643)
--- src/axios/url.ts
+++ src/axios/url.ts
@@ -1,8 +1,8 @@
 let url = ''
 switch (process.env.NODE_ENV) { 
-  case 'development': url = "http://10.21.1.104:9999"; break;
-  case 'test': url = "http://10.21.1.104:9999"; break;
-  case 'production': url = "http://10.21.1.104:9999"; break;
-  default:  url = "http://10.21.1.104:9999"; break;
+  case 'development': url = "http://127.0.0.1:9999"; break;
+  case 'test': url = "http://127.0.0.1:9999"; break;
+  case 'production': url = "http://127.0.0.1:9999"; break;
+  default:  url = "http://127.0.0.1:9999"; break;
 }
 export default url
